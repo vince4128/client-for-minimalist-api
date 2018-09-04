607bb98195fe973312cac0f8bf846e656bad0414
vince4128 <vrlt.vincent@gmail.com>
2018-09-04 17:44:50 +0200

    Root component added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
+
+import Root from './Root';
 import App from './components/App';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Route path="/" component={App}/>
-    </BrowserRouter>
+    <Root>
+        <BrowserRouter>        
+            <Route path="/" render={({ match }) => <App match={match} {...this.props} />}/>
+        </BrowserRouter>
+    </Root>
     , document.querySelector('#root')
 );
