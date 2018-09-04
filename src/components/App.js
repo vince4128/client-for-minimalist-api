12a663b05e9027184d4748c0c8719d024a5e6371
vince4128 <vrlt.vincent@gmail.com>
2018-09-04 10:19:02 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Route, Link} from 'react-router-dom';
+
+const App = () => {
+    return(
+        <div>App</div>
+    )
+}
+
+export default App;
