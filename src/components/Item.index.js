f53cbb4d40f46f9015a4832aa75d271e8901866d
vince4128 <vrlt.vincent@gmail.com>
2018-09-04 10:40:15 +0200

    ItemIndex component added

---
+++ src/components/Item.index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Route, Link} from 'react-router-dom';
+
+const ItemIndex = () => {
+    return(
+        <div>
+            Item Index
+        </div>
+    )
+}
+
+export default ItemIndex;
