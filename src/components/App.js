8606e3015f91b1df7cc2c925fa50fd85e494ad91
vince4128 <vrlt.vincent@gmail.com>
2018-09-04 10:39:54 +0200

    "/" Route added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import {Route, Link} from 'react-router-dom';
 
+import ItemIndex from './Item.index';
+
 const App = () => {
+
+
+
     return(
-        <div>App</div>
+        <div>App
+            <Route path="/" exact component={ItemIndex} />
+        </div>
     )
 }
 
