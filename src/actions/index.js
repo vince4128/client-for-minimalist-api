75fef7dc494da8dc5a695c294851e1960ddfd674
vince4128 <vrlt.vincent@gmail.com>
2018-09-06 17:32:36 +0200

    Subitem actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,14 @@ import {
     FETCH_ALL_ITEMS,
     FETCH_ITEM_BY_ID,
     CREATE_ITEM,
-    DELETE_ITEM
+    DELETE_ITEM,
+    FETCH_SUBITEMS,
+    DELETE_SUBITEM
 } from './types';
 
 const server = 'http://localhost:3000';
+
+/* ITEM */
 
 export function fetchAllItems(){
     const response = axios.get(server);
@@ -43,6 +47,51 @@ export function deleteItem(id){
 
     return {
         type: DELETE_ITEM,
+        payload: id
+    }
+}
+
+/* SUBITEM */
+
+export function fetchSubItems(){
+    alert('fetch subitems');
+    const response = axios.get(`${server}/subitem`);
+
+    response.then((r)=>{
+        alert('ok');
+        console.log(r.data);
+    })
+
+    return {
+        type:FETCH_SUBITEMS,
+        payload:response
+    }
+}
+
+export function fetchSubItem(id){
+    const response = axios.get(`${server}/subitem/${id}`);
+
+    return {
+        type:FETCH_ITEM_BY_ID,
+        payload:response
+    }
+}
+
+/*export function fetchSubItemByItem(id){
+    const response = axios.get(`${server}/${id}`);
+
+    return {
+        type:FETCH_ITEM_BY_ID,
+        payload:response
+    }
+}*/
+
+export function deleteSubItem(id){
+    
+    axios.delete(`${server}/subitem/${id}`);
+
+    return {
+        type: DELETE_SUBITEM,
         payload: id
     }
 }
