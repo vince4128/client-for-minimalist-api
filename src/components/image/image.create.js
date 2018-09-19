7e087bc1287dcea88ad52f67becd5b7d9525e3f0
vince4128 <vrlt.vincent@gmail.com>
2018-09-19 17:42:50 +0200

    auth and error handler added

--- src/components/image/image.create.js
+++ src/components/image/image.create.js
@@ -15,34 +15,102 @@ class ImageCreate extends Component {
         super(props);
         this.state = {
             imageFile: [],
+            optionalName: "",
+            errors:{
+                imageFile:"",
+                title:""
+            },
             uploadStatus: false
         }
         this.handleUploadImage = this.handleUploadImage.bind(this);
     }
 
     handleOnDrop = (newImageFile, rejectedFile) => {
-        console.log("onDrop !!! : ", newImageFile);
         this.setState({imageFile: newImageFile});        
     };
 
-    handleUploadImage(ev){
+    onInputChange = (optionalName) => {
+        this.setState({optionalName});
+        this.checkError();
+    }
 
-        alert('handleUploadImage !');
+    checkError = () => {
+        this.state.imageFile.length < 1 ? this.setError("file", true) : this.setError("file", false);
+        this.state.optionalName === "" ? this.setError("title", true) : this.setError("title", false);
 
+        if(this.state.imageFile.length < 1 || this.state.optionalName === ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    setError = (err, status) => {
+        if(err === "file"){
+            if(status){
+                this.setState(prevState => ({
+                    errors: {
+                        ...prevState.errors,
+                        imageFile: "pas de fichier"
+                    }
+                }))
+            }else{
+                this.setState(prevState => ({
+                    errors: {
+                        ...prevState.errors,
+                        imageFile: ""
+                    }
+                }))
+            }       
+        }else if(err === "title"){
+            if(status){
+                this.setState(prevState => ({
+                    errors: {
+                        ...prevState.errors,
+                        title: "pas de titre"
+                    }
+                }))
+            }else{
+                this.setState(prevState => ({
+                    errors: {
+                        ...prevState.errors,
+                        title: ""
+                    }
+                }))
+            }            
+        }
+    }
+
+    handleUploadImage = (ev) => {
         ev.preventDefault();
 
-        const data = new FormData();
-        data.append('file', this.uploadInput.files[0]);
-        data.append('filename', this.fileName.value);
+        if(this.checkError()){
 
-        alert('la requete va etre passee !');
+            alert('il y a des erreurs !');
 
-        axios.post('http://localhost:3000/upload', data)
-            .then((r)=>{
-                this.setState({ imageURL: `http://localhost:3000/${r.body.file}`, uploadStatus: true });
-            }).catch((err)=>{
-                console.log(err);
+        }else{
+
+            alert('handleUploadImage !');
+
+            const data = new FormData();
+            data.append('filename', this.state.optionalName);
+            data.append('file', this.state.imageFile[0]);   
+
+            alert('la requete va etre passee !');
+
+            axios.post('http://localhost:3000/upload', data, {
+                headers: {authorization: this.props.connected}
             })
+                .then((r)=>{
+                    //this.setState({ imageURL: `http://localhost:3000/${r.body.file}`, uploadStatus: true });
+                    //lancer action
+                    alert('callback');
+                    console.log('callback post image', r.data);
+                }).catch((err)=>{
+                    console.log(err);
+                })
+
+        }
 
     }
 
@@ -51,21 +119,29 @@ class ImageCreate extends Component {
             <div className="container">
             {JSON.stringify(this.state)}
                 <form onSubmit={this.handleUploadImage}>
+
                     <div className="form-group">
-                        <input className="form-controm" ref={(ref) => {this.uploadInput = ref;}} type="file"/>
-                    </div>
-                    <div className="form-group">
-                        <input className="form-control" ref={(ref) => {this.fileName = ref;}} type="text" placeholder="Optionnale name for the file"/>
+                        <lable>nom du fichier</lable>
+                        <input 
+                            className="form-control" 
+                            type="text" 
+                            placeholder="Optional name for the file" 
+                            value={this.state.optionalName}
+                            onChange={event => this.onInputChange(event.target.value)}
+                            />                        
+                        {this.state.errors.title?this.state.errors.title:""}                        
                     </div>
 
-                    {/*<Field
-                        ref={(ref) => {this.uploadInput = ref;}}
+                    <div className="form-group">
+                    <Field
                         name="uploadInput"
                         component={DropZoneField}
                         type="file"
                         imageFile={this.state.imageFile}
                         handleOnDrop={this.handleOnDrop}                    
-                    />*/}
+                    />
+                    {this.state.errors.imageFile?this.state.errors.imageFile:""}
+                    </div>
 
                     <button className="btn btn-success">Upload</button>
 
@@ -76,30 +152,14 @@ class ImageCreate extends Component {
 
 }
 
-function validate(values){
-
-    const errors = {};
-
-    // validate the inputs from 'values'
-    if(!values.title){
-        errors.title = "Enter a title !";
-    }
-
-    //if errors is empty, the form is fine to submit
-    //if errors as any property, redux form is invalid
-    return errors;
-
-}
-
 function mapStateToProps(state){
     return {auth:state.auth, categories:state.categories, images:state.images}
 }
 
 export default reduxForm({
-    validate:validate,
     form:'CreateImageForm'   //name must be unique (in case of several form it's usefull), and could be whatever string we want. 
 })(
-    withRouter(connect(mapStateToProps, {createImage})(ImageCreate))
+    withRouter(requireAuth(connect(mapStateToProps, {createImage})(ImageCreate)))
 );
 
 
